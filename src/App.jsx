8972c57c062d9cf2914da5b937a6cb6c3ac86b9a
wronah <115170466+wronah@users.noreply.github.com>
2024-06-09 07:20:10 +0200

    7 Main page with working select (#16)
    Co-authored-by: PanCamel <wbalucki18@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import './App.css'
-import CsvReader from './components/CsvReader'
+import "./App.css";
+import CsvReader from "./components/CsvReader";
 
 function App() {
-
   return (
     <div className="App">
-      <h1>CSV Reader</h1>
+      <h1 className="big-text">RAIL RIVALS</h1>
       <CsvReader />
+      <div className="elipse-1"></div>
+      <div className="elipse-2"></div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
