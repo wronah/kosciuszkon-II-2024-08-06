d8f6a5571cc047c247b9d51be221471a87d0b1f2
wronah <115170466+wronah@users.noreply.github.com>
2024-06-09 07:22:40 +0200

    Routing (#17)
    Co-authored-by: PanCamel <wbalucki18@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import CsvReader from "./components/CsvReader";
+import Stats from "./pages/Stats";
+import "./App.css";
 
-function App() {
+function Home() {
+  const navigate = useNavigate();
+  const goToNewPage = () => {
+    navigate("/stats");
+  };
   return (
     <div className="App">
       <h1 className="big-text">RAIL RIVALS</h1>
       <CsvReader />
       <div className="elipse-1"></div>
       <div className="elipse-2"></div>
+      <button onClick={goToNewPage} className="btn">
+        Go to Customer Page
+      </button>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/stats" element={<Stats />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Stats.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import React, { Component } from "react";
+
+export default class Stats extends Component {
+  render() {
+    return <div>Kutas</div>;
+  }
+}
